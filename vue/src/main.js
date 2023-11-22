6e9912ba494a090a886ebbf739f0bd60571656ee
w <Wuchongyangemail@tom.com>
2023-11-22 15:07:24 +0800

    no message

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,7 +4,7 @@ import router from './router/index'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 import 'dayjs/locale/zh-cn'
 import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
